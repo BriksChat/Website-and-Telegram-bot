ab45b2f8f8c8cd308d1540da824a0b5e69085104
BriksChat <GitHubBro@yandex.ru>
2026-08-17 20:59:00 +0300

    Change API and Telegram bot URLs for local use
    Update API URLs for local development.

--- website/config.js
+++ website/config.js
@@ -1,5 +1,5 @@
 /* Локальный API используется только при запуске на компьютере.
  * Перед публикацией на REG.RU укажите публичный HTTPS-адрес API.
  * Ссылку на Telegram добавляйте только после создания бота. */
-window.ENGLISH_API_URL = 'https://learenglish-api-chatbriks.waw0.amvera.tech';
-window.TELEGRAM_BOT_URL = 'https://t.me/MyLearEnglishBot';
+window.ENGLISH_API_URL = 'http://127.0.0.1:5000';
+window.TELEGRAM_BOT_URL = '';
